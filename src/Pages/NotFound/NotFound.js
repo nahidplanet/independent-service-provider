c6ae1f277e8ce3063801f1a55359eb93d5a2e747
nahidplanet <nahidplanet.bd@gmail.com>
2022-05-16 15:00:14 +0600

    404 page added

--- src/Pages/NotFound/NotFound.js
+++ src/Pages/NotFound/NotFound.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 
 const NotFound = () => {
+    const navigate=  useNavigate();
     return (
-        <div>
-            404 not fouond
+        <div className='w-full h-80vh cursor-pointer'>
+            <img onClick={()=>navigate("/home")} className='w-full h-full' src="https://i.ibb.co/2c7q9rB/x.jpg" alt="" />
         </div>
     );
 };
